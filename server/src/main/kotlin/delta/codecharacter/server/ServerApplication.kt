b87813cb8826a2f74df8915d1391bae01fced66f
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-02-08 23:59:20 +0530

    fix: lint and remove boilerplate

--- server/src/main/kotlin/delta/codecharacter/server/ServerApplication.kt
+++ server/src/main/kotlin/delta/codecharacter/server/ServerApplication.kt
@@ -1,26 +1,15 @@
 package delta.codecharacter.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.mongo.MongoProperties
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @SpringBootApplication
-class ServerApplication {
-    @Bean
-    fun embeddedMongoAutoConfiguration(
-        mongoProperties: MongoProperties?
-    ): EmbeddedMongoAutoConfiguration? {
-        return EmbeddedMongoAutoConfiguration(mongoProperties)
-    }
-}
+class ServerApplication
 
 fun main(args: Array<String>) {
-    System.setProperty("os.arch", "x86_64")
     runApplication<ServerApplication>(*args)
 }
