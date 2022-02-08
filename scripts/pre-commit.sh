17b8832ab6b31dc2d1dd5cdafccd278c80f160d7
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-02-08 08:41:39 +0530

    feat: Add run configuration and modify pre-commit

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,7 +2,15 @@
 
 echo "Running pre-commit checks..."
 
-./gradlew check test --daemon
+TOP_DIR=$(git rev-parse --show-toplevel)
+
+$TOP_DIR/gradlew spotlessApply --daemon
+
+FILES=$((git diff --cached --name-only --diff-filter=ACM) || true)
+
+git add $(echo $FILES | paste -sd " " -)
+
+$TOP_DIR/gradlew test --daemon
 
 status=$?
 
