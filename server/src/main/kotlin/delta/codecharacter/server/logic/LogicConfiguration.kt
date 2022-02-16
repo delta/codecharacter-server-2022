5c6fea37791a98bcb20a60a2f2979ef5f69274a0
Dipesh Kafle <dipesh.kaphle111@gmail.com>
2022-02-16 09:38:31 +0545

    Add: Winner deciding mechanishm

--- server/src/main/kotlin/delta/codecharacter/server/logic/LogicConfiguration.kt
+++ server/src/main/kotlin/delta/codecharacter/server/logic/LogicConfiguration.kt
@@ -3,8 +3,8 @@ package delta.codecharacter.server.logic
 import delta.codecharacter.server.logic.rating.RatingAlgorithm
 import delta.codecharacter.server.logic.rating.ZeroRatingAlgorithm
 import delta.codecharacter.server.logic.validation.MapValidator
-import delta.codecharacter.server.logic.verdict.Player1WinnerAlgorithm
 import delta.codecharacter.server.logic.verdict.VerdictAlgorithm
+import delta.codecharacter.server.logic.verdict.WinnerAlgorithm
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -18,7 +18,7 @@ class LogicConfiguration {
 
     @Bean
     fun verdictAlgorithm(): VerdictAlgorithm {
-        return Player1WinnerAlgorithm()
+        return WinnerAlgorithm()
     }
 
     @Bean
