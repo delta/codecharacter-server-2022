b03071e8cd1f964035fbd2ef700c5fe4b8aeb0b5
Ram-20062003 <ramajs2003@gmail.com>
2022-02-18 14:22:54 +0530

    fix: gradle format

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
     // jwt token library
-    implementation ("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
+    implementation("org.springframework.security:spring-security-jwt:1.1.1.RELEASE")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client:2.6.3")
 
