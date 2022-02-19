881dc70ffe78eee2411a7b075dfe56ffb371de5a
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-02-19 23:41:56 +0530

    Fix response for unauthorized

--- server/src/main/kotlin/delta/codecharacter/server/auth/AuthService.kt
+++ server/src/main/kotlin/delta/codecharacter/server/auth/AuthService.kt
@@ -3,9 +3,11 @@ package delta.codecharacter.server.auth
 import delta.codecharacter.dtos.PasswordLoginRequestDto
 import delta.codecharacter.dtos.PasswordLoginResponseDto
 import delta.codecharacter.server.exception.CustomException
+import delta.codecharacter.server.user.UserEntity
 import delta.codecharacter.server.user.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
@@ -18,9 +20,14 @@ class AuthService(
     fun passwordLogin(passwordLoginRequestDto: PasswordLoginRequestDto): PasswordLoginResponseDto {
         val email = passwordLoginRequestDto.email
         val password = passwordLoginRequestDto.password
-        val user = userService.loadUserByUsername(email)
+        val user: UserEntity
+        try {
+            user = userService.loadUserByUsername(email)
+        } catch (e: UsernameNotFoundException) {
+            throw CustomException(HttpStatus.UNAUTHORIZED, "Invalid email or password")
+        }
         if (passwordEncoder.matches(password, user.password)) {
             return PasswordLoginResponseDto(authUtil.generateToken(user))
-        } else throw CustomException(HttpStatus.NOT_FOUND, "Invalid Password")
+        } else throw CustomException(HttpStatus.UNAUTHORIZED, "Invalid credentials")
     }
 }
