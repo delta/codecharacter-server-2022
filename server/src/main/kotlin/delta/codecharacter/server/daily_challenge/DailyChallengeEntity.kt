c55209e68c8e5d9e66000c7aab82b12534f0a14e
zabarudo <smartkevin2003@gmail.com>
2023-01-12 16:10:08 +0530

    fix: added id to dcEntity, made dcObject

--- server/src/main/kotlin/delta/codecharacter/server/daily_challenge/DailyChallengeEntity.kt
+++ server/src/main/kotlin/delta/codecharacter/server/daily_challenge/DailyChallengeEntity.kt
@@ -1,13 +1,17 @@
 package delta.codecharacter.server.daily_challenge
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import delta.codecharacter.dtos.ChallengeTypeDto
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.util.UUID
 
 @Document(collection = "daily_challenges")
 data class DailyChallengeEntity(
-    val day: Int,
-    val challName: String,
-    val challType: ChallengeTypeDto,
-    val chall: String,
-    val description: String?,
+        @Id val id: UUID,
+        @field:JsonProperty("day") val day: Int,
+        @field:JsonProperty("challName") val challName: String,
+        @field:JsonProperty("challType") val challType: ChallengeTypeDto,
+        @field:JsonProperty("chall") val chall: String,
+        @field:JsonProperty("description") val description: String?,
 )
--- server/src/main/kotlin/delta/codecharacter/server/seeders/DailyChallengeSeeder.kt
+++ server/src/main/kotlin/delta/codecharacter/server/seeders/DailyChallengeSeeder.kt
@@ -1,13 +1,17 @@
-package delta.codecharacter.server.daily_challenge
+package delta.codecharacter.server.seeders
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import delta.codecharacter.server.daily_challenge.DailyChallengeEntity
+import delta.codecharacter.server.daily_challenge.DailyChallengeObject
+import delta.codecharacter.server.daily_challenge.DailyChallengeRepository
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import java.util.UUID
 
 @Component
 class DailyChallengeSeeder {
@@ -24,8 +28,23 @@ class DailyChallengeSeeder {
             val jsonString = this::class.java.classLoader.getResource("dcConstants.json")?.readText()
             if (!jsonString.isNullOrEmpty()) {
                 val objectMapper = jacksonObjectMapper()
-                val dcs: List<DailyChallengeEntity> = objectMapper.readValue(jsonString)
-                dailyChallengeRepository.saveAll(dcs)
+                val dcs: List<DailyChallengeObject> = objectMapper.readValue(jsonString)
+                var dcEntities: List<DailyChallengeEntity> = listOf()
+                dcs.forEach {
+                    val id = UUID.randomUUID()
+                    dcEntities =
+                        dcEntities.plus(
+                            DailyChallengeEntity(
+                                id = id,
+                                day = it.day,
+                                chall = it.chall,
+                                challName = it.challName,
+                                challType = it.challType,
+                                description = it.description
+                            )
+                        )
+                }
+                dailyChallengeRepository.saveAll(dcEntities)
             } else {
                 logger.error("dcConstants.json not found or doesn't exist")
             }
