7c47afe93617a76e84fccf9a28941c91b5b1f07f
zabarudo <smartkevin2003@gmail.com>
2023-01-12 16:20:14 +0530

    fix: fixed formatting for dcEntity, dcObject

--- server/src/main/kotlin/delta/codecharacter/server/daily_challenge/DailyChallengeEntity.kt
+++ server/src/main/kotlin/delta/codecharacter/server/daily_challenge/DailyChallengeEntity.kt
@@ -8,10 +8,10 @@ import java.util.UUID
 
 @Document(collection = "daily_challenges")
 data class DailyChallengeEntity(
-        @Id val id: UUID,
-        @field:JsonProperty("day") val day: Int,
-        @field:JsonProperty("challName") val challName: String,
-        @field:JsonProperty("challType") val challType: ChallengeTypeDto,
-        @field:JsonProperty("chall") val chall: String,
-        @field:JsonProperty("description") val description: String?,
+    @Id val id: UUID,
+    @field:JsonProperty("day") val day: Int,
+    @field:JsonProperty("challName") val challName: String,
+    @field:JsonProperty("challType") val challType: ChallengeTypeDto,
+    @field:JsonProperty("chall") val chall: String,
+    @field:JsonProperty("description") val description: String?,
 )
--- server/src/main/kotlin/delta/codecharacter/server/daily_challenge/DailyChallengeObject.kt
+++ server/src/main/kotlin/delta/codecharacter/server/daily_challenge/DailyChallengeObject.kt
@@ -1,9 +1,9 @@
 package delta.codecharacter.server.daily_challenge
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import delta.codecharacter.dtos.ChallengeTypeDto
 
-data class DailyChallengeObject(val day: Int,
+data class DailyChallengeObject(
+    val day: Int,
     val challName: String,
     val challType: ChallengeTypeDto,
     val chall: String,
