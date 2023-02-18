56363ca560ef552f50198b2b183f809f8fa68c54
b1otch <91837072+b1otch@users.noreply.github.com>
2023-02-19 00:16:05 +0530

    update(Update game entities): Updated Attackers and Defenders (#17)
    * update(updated game entities): Updated Attackers and Defenders, Added aerial attribute
    
    * fix: Re-Add deleted hooks
    
    * fix(Attacker and Defender attributes): Updated attribute names
    
    * fix: namings for game-parameters
    
    ---------
    
    Co-authored-by: Ram-20062003 <ramajs2003@gmail.com>
    Co-authored-by: Ram Ganesh K. R <83528327+Ram-20062003@users.noreply.github.com>

--- server/src/main/kotlin/delta/codecharacter/server/config/GameConfiguration.kt
+++ server/src/main/kotlin/delta/codecharacter/server/config/GameConfiguration.kt
@@ -14,13 +14,39 @@ class GameConfiguration {
         return GameParameters(
             attackers =
             setOf(
-                Attacker(id = 1, hp = 10, range = 3, attackPower = 3, speed = 3, price = 1),
-                Attacker(id = 2, hp = 10, range = 3, attackPower = 3, speed = 3, price = 1),
+                Attacker(
+                    id = 1,
+                    hp = 10,
+                    range = 2,
+                    attackPower = 4,
+                    speed = 4,
+                    price = 2,
+                    aerial = false,
+                ),
+                Attacker(
+                    id = 2,
+                    hp = 20,
+                    range = 4,
+                    attackPower = 2,
+                    speed = 2,
+                    price = 2,
+                    aerial = false,
+                ),
+                Attacker(
+                    id = 3,
+                    hp = 15,
+                    range = 6,
+                    attackPower = 2,
+                    speed = 6,
+                    price = 4,
+                    aerial = true,
+                ),
             ),
             defenders =
             setOf(
-                Defender(id = 1, hp = 10, range = 4, attackPower = 5, price = 1),
-                Defender(id = 2, hp = 10, range = 6, attackPower = 5, price = 1),
+                Defender(id = 1, hp = 400, range = 4, attackPower = 10, price = 25, aerial = false),
+                Defender(id = 2, hp = 600, range = 6, attackPower = 20, price = 50, aerial = false),
+                Defender(id = 3, hp = 400, range = 6, attackPower = 5, price = 75, aerial = true),
             ),
             numberOfTurns = 500,
             numberOfCoins = 1000,
